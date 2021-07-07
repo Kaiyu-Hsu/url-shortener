18f048f43736a06455c089a25404ee869b6f2e75
Kaiyu <anne550323@gmail.com>
2021-07-07 18:56:49 +0800

    feat: add url seeder

---
+++ models/urlSeeder.js
@@ -0,0 +1,39 @@
+const URL = require('./urlSchema')
+const db = require('../config/mongoose')
+
+// basic information
+const shortUrlLength = 5
+const basicUrl = 'https://url-shortener.herokuapp.com/'
+
+// all letters & numbers
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const upperCaseLetters = lowerCaseLetters.toUpperCase()
+const numbers = '0123456789'
+const allCollection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
+
+let randomText = ''
+let randomIndex = 0
+
+function generateRandomUrl() {
+  for (let i = 0; i < shortUrlLength; i++) {
+    randomIndex = Math.floor(Math.random() * allCollection.length)
+    randomText += allCollection[randomIndex]
+  }
+  return shortUrl = basicUrl + randomText
+}
+
+db.once('open', () => {
+  URL.create(
+    {
+      originalUrl: basicUrl,
+      shortenUrl: generateRandomUrl()
+    }
+  )
+    .then(() => {
+      console.log('Insert categories done!')
+      return db.close()
+    })
+    .then(() => {
+      console.log('Database connection close.')
+    })
+})
