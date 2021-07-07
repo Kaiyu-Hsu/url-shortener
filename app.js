5a78524296dceab41923e3e149d7d0d15c5f4ecb
Kaiyu <anne550323@gmail.com>
2021-07-07 13:44:18 +0800

    feat: creat app.js basic setting

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+const POST = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.listen(PORT, () => {
+  console.log(`APP is working on http://localhost:${PORT}`)
+})
