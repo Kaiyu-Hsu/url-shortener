606890f14aec70885a17352dcfef84a8557332c5
Kaiyu <anne550323@gmail.com>
2021-07-07 16:09:42 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser')
 require('./config/mongoose')
 
 const app = express()
-const POST = 3000
+const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(PORT, () => {
   console.log(`APP is working on http://localhost:${PORT}`)
