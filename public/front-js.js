c1193ed54d09a77559aa2f4ce89d64c27c946b12
Kaiyu <anne550323@gmail.com>
2021-07-08 16:32:17 +0800

    feat: add modal detail

--- public/front-js.js
+++ public/front-js.js
@@ -2,17 +2,12 @@ const copyBtn = document.querySelector('#copyBtn')
 const copyUrl = document.querySelector('#randomUrl')
 
 function copy() {
-  console.log(copyUrl)
-  window.getSelection(copyUrl)
+  window.getSelection(copyUrl.innerHTML)
   document.execCommand("copy")
-  alert("複製成功!")
-  console.log('end')
 }
 
 copyBtn.addEventListener('click', event => {
-  
   const target = event.target
-  console.log(target)
   if(target) {
     copy()
   }
