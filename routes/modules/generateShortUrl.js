d77e9a8770d2f39860c850ae9284ff53ae7db9c3
Kaiyu <anne550323@gmail.com>
2021-07-10 14:07:12 +0800

    feat: add routes of index & generateShortUrl

---
+++ routes/modules/generateShortUrl.js
@@ -0,0 +1,22 @@
+// basic information
+const shortUrlLength = 5
+
+// all letters & numbers
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const upperCaseLetters = lowerCaseLetters.toUpperCase()
+const numbers = '0123456789'
+const allCollection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
+
+let randomText = ''
+let randomIndex = 0
+let shortUrl = ''
+
+function generateRandomUrl() {
+  for (let i = 0; i < shortUrlLength; i++) {
+    randomIndex = Math.floor(Math.random() * allCollection.length)
+    randomText += allCollection[randomIndex]
+  }
+  return shortUrl += randomText
+}
+
+module.exports = generateRandomUrl()
