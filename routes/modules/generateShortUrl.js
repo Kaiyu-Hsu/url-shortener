6d03e9a9ee9f233eecc2d5829b328592e9725a41
Kaiyu <anne550323@gmail.com>
2021-07-11 17:21:48 +0800

    feat: 修改generateRandomUrl () 重複生產的問題

--- routes/modules/generateShortUrl.js
+++ routes/modules/generateShortUrl.js
@@ -1,17 +1,16 @@
-// basic information
-const shortUrlLength = 5
-
-// all letters & numbers
-const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
-const upperCaseLetters = lowerCaseLetters.toUpperCase()
-const numbers = '0123456789'
-const allCollection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
-
-let randomText = ''
-let randomIndex = 0
-let shortUrl = ''
-
 function generateRandomUrl() {
+  // basic information
+  const shortUrlLength = 5
+
+  // all letters & numbers
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '0123456789'
+  const allCollection = (lowerCaseLetters + upperCaseLetters + numbers).split('')
+
+  let randomText = ''
+  let randomIndex = 0
+  let shortUrl = ''
   for (let i = 0; i < shortUrlLength; i++) {
     randomIndex = Math.floor(Math.random() * allCollection.length)
     randomText += allCollection[randomIndex]
